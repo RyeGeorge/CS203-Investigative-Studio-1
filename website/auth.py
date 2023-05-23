d03cdcff21814153f5d4c6a2bde15ba6dde1e919
MrCooperGit <100725634+MrCooperGit@users.noreply.github.com>
2023-05-24 11:34:34 +1200

    modified:   website/auth.py 	modified:   website/templates/forum.html commit

--- website/auth.py
+++ website/auth.py
@@ -61,7 +61,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account created!', category='success')
+            flash('Account created! Welcome ' + first_name, category='success')
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
